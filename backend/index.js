c79e18f88d1a0a4c4270185adc5106ecff30d90e
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-16 21:20:04 -0700

    can add comments

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,44 @@ app.put('/posts/:id/hug', async (req, res) => {
     }
 });
 
+app.post('/posts/:postId/comments', async (req, res) => {
+    const { postId } = req.params;
+    const { parent_id } = req.body;
+    const newComment = {
+        created_at: new Date().toISOString(), ...req.body
+    };
+
+    try {
+        const url = `${JSON_SERVER_URL}/${postId}`
+        const postResponse = await axios.get(url);
+        const post = postResponse.data;
+
+        if (parent_id !== undefined && parent_id !== null) {
+            const parentComment = post.comments[parent_id];
+            if (parentComment && parentComment.parent_id !== null) {
+                return res.status(400).send('Comments cannot be nested more than two levels deep.');
+            }
+        }
+
+        let maxCommentId = 0;
+
+        if (post.comments) {
+            Object.values(post.comments).forEach(comment => {
+                maxCommentId = Math.max(maxCommentId, comment.id);
+            });
+        }
+
+        newComment.id = maxCommentId + 1;
+
+        const updatedComments = { ...post.comments, [newComment.id]: newComment };
+        await axios.patch(`${JSON_SERVER_URL}/${postId}`, { comments: updatedComments });
+
+        res.json({ message: 'Comment added successfully', comment: newComment });
+    } catch (error) {
+        console.error('Error adding new comment:', error);
+        res.status(500).send('An error occurred while adding the comment');
+    }
+});
 
 
 app.listen(PORT, () => {
