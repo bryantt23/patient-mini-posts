ca1191f3d866671688fcb1a54dc67f3bbaa61273
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-16 23:13:43 -0700

    add infinite scroll

--- frontend/src/components/Posts.tsx
+++ frontend/src/components/Posts.tsx
@@ -1,22 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { fetchPosts } from '../services/patientInfo';
 import Post from './Post';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const PAGE_SIZE = 3;
 const Posts = () => {
+  const [index, setIndex] = useState(0);
   const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchData = async () => {
+    const postsData = await fetchPosts(index, PAGE_SIZE);
+    if (postsData.length === 0 || postsData.length < PAGE_SIZE) {
+      setHasMore(false);
+    } else {
+      setIndex(prevIndex => prevIndex + PAGE_SIZE);
+      setPosts(prevPosts => [...prevPosts, ...postsData]);
+    }
+  };
 
   useEffect(() => {
-    const loadPosts = async () => {
-      const postsData = await fetchPosts(0, 3);
-      setPosts(postsData);
-    };
-    loadPosts();
+    fetchData();
   }, []);
 
   return (
     <div>
-      {posts.map(post => (
-        <Post post={post} />
-      ))}
+      <InfiniteScroll
+        dataLength={posts.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {/* {posts} */}
+        {posts.map(post => (
+          <Post key={post.id} post={post} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 };
