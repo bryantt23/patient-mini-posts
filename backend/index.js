1a14f9bd9f8783e2be1febaa626eabc5189d1647
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-16 17:22:43 -0700

    serve posts

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
+const axios = require('axios')
 const express = require('express');
 
 const app = express();
 const PORT = 3000;
+const JSON_SERVER_URL = 'http://localhost:3001/posts'
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/posts', async (req, res) => {
+    try {
+        const response = await axios.get(JSON_SERVER_URL);
+        console.log("🚀 ~ app.get ~ response:", response)
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        res.status(500).send('An error occurred fetching the posts');
+    }
 });
 
 app.listen(PORT, () => {
